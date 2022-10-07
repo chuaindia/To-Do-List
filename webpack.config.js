aeb69f1acbe5619a30153b9bbba526293cdfbdb6
Tanusri Ghosh <tghosh25@gmail.com>
2022-10-07 16:28:31 +0530

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,28 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-module.exports = {
-	mode: "development",
-	entry: './src/index.js',
-	output: {
-		filename: 'main.js',
-		path: path.resolve(__dirname, 'dist'),
-	},
-
-	module: {
-		rules: [
-			{
-				test: /\.css$/i,
-				use: ['style-loader', 'css-loader'],
-			},
-		],
-	},
-
-	devServer: {
-		static: './dist',
-	},
-
-	plugins: [
-		new HtmlWebpackPlugin({
-			template: './src/index.html',
-		}),
-	],
-};
+ module.exports = {
+  mode: 'development',
+   entry: {
+     index: './src/index.js',
+     print: './src/print.js',
+   },
+   devtool: 'inline-source-map',
+   devServer: {
+    static: './dist',
+  },
+   plugins: [
+     new HtmlWebpackPlugin({
+      title: 'Development',
+     }),
+   ],
+   output: {
+     filename: '[name].bundle.js',
+     path: path.resolve(__dirname, 'dist'),
+     clean: true,
+     publicPath: '/',
+   },
+   optimization: {
+   runtimeChunk: 'single',
+  },
+ };
