7504180b51920a1bb24fbcb6efb9c7e311a875f7
Tanusri Ghosh <tghosh25@gmail.com>
2022-10-07 16:40:14 +0530

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,79 +1,18 @@
 import _ from 'lodash';
-import './style.css';
+import printMe from './print.js';
 
-const tasks = [
-	{
-		description: 'Morning Walk',
-		completed: true,
-		index: 0
-	},
-	{
-		description: 'Prepare Breakfast',
-		completed: true,
-		index: 1
-	},
-	{
-		description: 'Go shopping: ',
-		completed: true,
-		index: 2
-	},
-	{
-		description: 'Prepare Lunch',
-		completed: true,
-		index: 3
-	},
-	{
-		description: 'Learning Microverse materials',
-		completed: true,
-		index: 4
-	},
-	{
-		description: 'Do the unfinished activities',
-		completed: true,
-		index: 5
-	},
-	{
-		description: 'Shopping',
-		completed: true,
-		index: 6
-	},
-	{
-		description: 'Prepare Dinner',
-		completed: true,
-		index: 7
-	},
-	{
-		description: 'Evening Walk',
-		completed: true,
-		index: 8
-	},
-	{
-		description: 'Prayer',
-		completed: true,
-		index: 9
-	},
-	{
-		description: 'Reading',
-		completed: true,
-		index: 8
-	}
+ function component() {
+   const element = document.createElement('div');
+  const btn = document.createElement('button');
 
-]
+   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
 
-const myContainer = document.getElementById('to-do-list');
-tasks.forEach((item) => {
-	myContainer.innerHTML += `
-	<ul class='list-container'>
-		<li class='list-items'>
-		<input type="checkbox" id="check">
-		${item.description}
-		</li>
-	</ul>
-`;
-});
+  btn.innerHTML = 'Click me and check the console!';
+  btn.onclick = printMe;
 
-myContainer.innerHTML += `
-<div class='footer-list'>
-	<a href='#' class='footer-a'>Clear all completed</a>
-</div>
-`;
+  element.appendChild(btn);
+
+   return element;
+ }
+
+ document.body.appendChild(component())
