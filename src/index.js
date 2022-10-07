7c2b6b4d2c8e831c54bb8b3c8578489f0a9acdb7
Tanusri Ghosh <tghosh25@gmail.com>
2022-10-07 19:02:24 +0530

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,18 +1,50 @@
-import _ from 'lodash';
-import printMe from './print.js';
+import './main.css';
+import clearListContainer from './utils/clear-container.js';
 
- function component() {
-   const element = document.createElement('div');
-  const btn = document.createElement('button');
+const listContainer = document.querySelector('[data-lists]');
 
-   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+const todos = [
+  {
+    description: 'Cooking lunch ',
+    completed: false,
+    index: 1,
+  },
+  {
+    description: 'Join Microverse program',
+    completed: false,
+    index: 2,
+  },
+  {
+    description: 'Evening Walk',
+    completed: false,
+    index: 3,
+  },
+  {
+    description: 'Watch the walking dead season 11 Finale',
+    completed: false,
+    index: 4,
+  },
+];
 
-  btn.innerHTML = 'Click me and check the console!';
-  btn.onclick = printMe;
+const handleRender = () => {
+  clearListContainer(listContainer);
+  todos.forEach((todo) => {
+    const listElement = document.createElement('li');
+    listElement.classList.add('todo');
+    listElement.innerHTML = `
+      <div class="toggle">
+        <span class="material-icons box">check_box_outline_blank</span>
+        <div class="check-div">
+          <span class="material-icons check">done</span>
+        </div>
+      </div>
+      <div class="view">
+        <p>${todo.description}</p>
+      </div>
+      <span class="material-icons move-icon">more_vert</span>
+    `;
+    listContainer.appendChild(listElement);
+  });
+};
 
-  element.appendChild(btn);
-
-   return element;
- }
-
- document.body.appendChild(component())
+handleRender();
